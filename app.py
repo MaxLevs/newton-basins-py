4edd9a7a20b9019a9361a7d90d032f29404c904b
Liven Maxim <maxlevs@mail.ru>
2025-05-26 22:31:16 +0300

    feat: some UI modifications

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 from PIL import ImageTk
 
 from application.basins_drawer_service import BasinsDrawerService
-from domain.image_tile import ImageTile
 
 
 class MainApplication(tk.Frame):
@@ -11,15 +10,22 @@ class MainApplication(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
+        self.roots = [complex(1.2, -4), complex(3, 2.4), complex(-1.2, - 1.4),]
+        self.drawer = BasinsDrawerService(self.roots)
+
+        self.canvas = tk.Canvas(app, width=app.winfo_width(), height=app.winfo_height())
+        self.canvas.pack(side="top", fill="both", expand=True)
+
+        tk.Button(self, text='Calculate', command=self.draw).pack(side="right")
+
+    def draw(self):
+        image = self.drawer.draw(0, 0, 1)
+        tk_image = ImageTk.PhotoImage(image, master=self)
+        kk = self.canvas.create_image(image.size[0] / 2, image.size[1] / 2, image=tk_image)
+
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    image_size = ImageTile.image_size
-    root.geometry(f'{image_size[0]*2}x{image_size[1]*2}')
-    MainApplication(root).pack(side="top", fill="both", expand=True)
-    canvas = tk.Canvas(root, width=image_size[0]*2, height=image_size[1]*2)
-    drawer = BasinsDrawerService([complex(1.2, -4), complex(3, 2.4), complex(-1.2, - 1.4),])
-    image = drawer.draw(0, 0, 1).resize((image_size[0]*2, image_size[1]*2))
-    tk_image = ImageTk.PhotoImage(image, master=root)
-    kk = canvas.create_image(image_size[0] / 2, image_size[1] / 2, image=tk_image)
-    canvas.pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    app = tk.Tk()
+    app.geometry(f'512x512')
+    MainApplication(app).pack(side="top", fill="both", expand=True)
+    app.mainloop()
