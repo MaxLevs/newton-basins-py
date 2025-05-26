40f39a2f00954766307c560dcc533c9fe128335c
Liven Maxim <maxlevs@mail.ru>
2025-05-26 22:56:36 +0300

    feat: some UI modifications

--- app.py
+++ app.py
@@ -3,29 +3,33 @@
 from PIL import ImageTk
 
 from application.basins_drawer_service import BasinsDrawerService
+from domain.image_tile import ImageTile
 
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-
-        self.roots = [complex(1.2, -4), complex(3, 2.4), complex(-1.2, - 1.4),]
-        self.drawer = BasinsDrawerService(self.roots)
-
-        self.canvas = tk.Canvas(app, width=app.winfo_width(), height=app.winfo_height())
+        tk.Button(self, text='Calculate', command=self.draw).pack(side="top")
+        self.canvas = tk.Canvas(self, width=512, height=512)
         self.canvas.pack(side="top", fill="both", expand=True)
 
-        tk.Button(self, text='Calculate', command=self.draw).pack(side="right")
+        self.roots = [complex(1.2, -4), complex(3, 2.4), complex(-1.2, - 1.4),]
+        self.tk_images = []
+        self.drawer = BasinsDrawerService(self.roots)
+
 
     def draw(self):
         image = self.drawer.draw(0, 0, 1)
         tk_image = ImageTk.PhotoImage(image, master=self)
-        kk = self.canvas.create_image(image.size[0] / 2, image.size[1] / 2, image=tk_image)
+        self.canvas.create_image(image.size[0] / 2, image.size[1] / 2, image=tk_image)
+        self.tk_images.append(tk_image)
 
 
 if __name__ == "__main__":
-    app = tk.Tk()
-    app.geometry(f'512x512')
-    MainApplication(app).pack(side="top", fill="both", expand=True)
-    app.mainloop()
+    tk_root = tk.Tk()
+    tk_root.geometry(f'512x512')
+    app = MainApplication(tk_root)
+    app.pack(side="top", fill="both", expand=True)
+
+    tk_root.mainloop()
