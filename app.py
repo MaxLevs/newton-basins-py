ed8a0cd0efbd22ca8eca2438717f3c53883bdd0d
Liven Maxim <maxlevs@mail.ru>
2025-05-27 05:52:19 +0300

    feat: UI improvements

--- app.py
+++ app.py
@@ -11,16 +11,23 @@ class ControlUnit(tk.Frame):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        tk.Button(self, text='Move Left', command=parent.move_left).pack(side="left")
-        tk.Button(self, text='Calculate', command=parent.draw).pack(side="left")
-        tk.Button(self, text='Move Right', command=parent.move_right).pack(side="left")
+
+    # noinspection PyUnresolvedReferences
+    def place(self, func):
+        func(self)
+        return self
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
-        ControlUnit(self, *args, **kwargs).pack(side="top")
+        cu = ControlUnit(self, *args, **kwargs)
+        cu.place(lambda itself: tk.Button(itself, text='Move Left', command=self.move_left).pack(side="left"))
+        cu.place(lambda itself: tk.Button(itself, text='Calculate', command=self.draw).pack(side="left"))
+        cu.place(lambda itself: tk.Button(itself, text='Move Right', command=self.move_right).pack(side="left"))
+        cu.place(lambda itself: tk.Button(itself, text='Clear All', command=self.clear_elements).pack(side="left"))
+        cu.pack(side="top")
 
         self.canvas = tk.Canvas(self, width=parent.winfo_width(), height=parent.winfo_width())
         self.canvas.pack(side="top", fill="both", expand=True)
@@ -33,19 +40,18 @@ class MainApplication(tk.Frame):
     def draw(self):
         image = self.drawer.draw(0, 0, 1)
         tk_image = ImageTk.PhotoImage(image, master=self)
-        element_info = self.canvas.create_image(image.size[0], image.size[1], image=tk_image)
-        self.tk_images.append((tk_image, element_info))
+        self.canvas.create_image(image.size[0], image.size[1], image=tk_image, tag="basins_tiles")
+        self.tk_images.append(tk_image)
 
     def move_left(self):
-        for img in self.tk_images:
-            _, canvas_element_info = img
-            # self.canvas.itemconfig(img, state="hidden")
-            self.canvas.move(canvas_element_info, -10, 0)
+        # self.canvas.itemconfig(img, state="hidden")
+        self.canvas.move("basins_tiles", -10, 0)
 
     def move_right(self):
-        for img in self.tk_images:
-            _, canvas_element_info = img
-            self.canvas.move(canvas_element_info, 10, 0)
+        self.canvas.move("basins_tiles", 10, 0)
+
+    def clear_elements(self):
+        self.tk_images = []
 
 
 if __name__ == "__main__":
