ef10eb6ac869f9f565bbac73b2f505221f8564b9
Liven.Maksim <Maksim.Liven@infotecs.ru>
2025-05-28 10:23:51 +0300

    feat: positioning

--- gui/space.py
+++ gui/space.py
@@ -1,10 +1,12 @@
 ﻿from tkinter import Frame, Canvas, ALL
 from typing import List
+import math
 
 from PIL import ImageTk
 
 from application.basins_drawer_service import BasinsDrawerService
 from domain.image_tile import ImageTile
+from gui.ui_tile import UiTile
 
 
 class MovementCalculator:
@@ -29,17 +31,19 @@ class MovementCalculator:
 class Space(Frame):
     TILES_TAG = "basins_tiles"
 
-    def __init__(self, parent, drawer : BasinsDrawerService, *args, **kwargs):
+    def __init__(self, parent, drawer: BasinsDrawerService, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
 
         self.movement_calculator = MovementCalculator()
+        self.basins_drawer = drawer
+
         self.x = 0
         self.y = 0
         self.zoom = 1
-        self.temp_tile = ImageTile(self.x, self.y, self.zoom, drawer)
 
-        self.images: List[ImageTk.PhotoImage] = []
+        self.tiles: List[UiTile] = []
+
         self.canvas = Canvas(self, width=parent.winfo_width(), height=parent.winfo_width())
         self.canvas.bind('<B1-Motion>', self.drag)
         self.canvas.bind('<B1-ButtonRelease>', lambda _: self.movement_calculator.reset())
@@ -58,7 +62,6 @@ class Space(Frame):
         self.move(dx, dy)
 
     def move(self, x, y):
-        # self.canvas.itemconfig(img, state="hidden")
         self.x = x
         self.y = y
         self.canvas.move(ALL, x, y)
@@ -69,11 +72,32 @@ class Space(Frame):
     def move_right(self):
         self.move(10, 0)
 
+    def temp_generate_position_factions(self, n: int):
+        for i in range(n):
+            yield i
+            if i != 0:
+                yield -i
+
+    def temp_create_tiles(self):
+        screen_x = self.winfo_width()
+        screen_y = self.winfo_height()
+        gen_x = list(self.temp_generate_position_factions(math.ceil(screen_x / ImageTile.image_x_max)))
+        gen_y = list(self.temp_generate_position_factions(math.ceil(screen_y / ImageTile.image_y_max)))
+        center_x = screen_x / 2
+        center_y = screen_y / 2
+        coords = [(center_x + fraction_x * ImageTile.image_x_max, center_y + fraction_y * ImageTile.image_y_max) for fraction_x in gen_x for fraction_y in gen_y]
+        for x, y in coords:
+            image_tile = ImageTile((x - center_y) / ImageTile.basic_zoom, -(y - center_y) / ImageTile.basic_zoom, 1, self.basins_drawer)
+            self.tiles.append(UiTile(self.canvas, image_tile, (x, y)))
+
     def clear_elements(self):
-        self.images = []
+        for tile in self.tiles:
+            tile.clear()
 
     def update(self):
-        image = self.temp_tile.draw()
-        image_tk = ImageTk.PhotoImage(image, master=self)
-        self.canvas.create_image(self.winfo_width() / 2, self.winfo_height() / 2, image=image_tk, tag=Space.TILES_TAG)
-        self.images.append(image_tk)
+        if not self.tiles:
+            self.temp_create_tiles()
+
+        for tile in self.tiles:
+            image_tk = tile.update()
+            self.canvas.create_image(tile.screen_position[0], tile.screen_position[1], image=image_tk, tag=Space.TILES_TAG)
---
+++ gui/ui_tile.py
@@ -0,0 +1,21 @@
+﻿import tkinter as tk
+
+from PIL import ImageTk
+
+from domain.image_tile import ImageTile
+
+
+class UiTile:
+    def __init__(self, parent: tk.Canvas, image_tile: ImageTile, screen_position: (float, float)):
+        self.parent = parent
+        self.image_tile = image_tile
+        self.image_tk = None
+        self.screen_position = screen_position
+
+    def update(self):
+        image = self.image_tile.draw()
+        self.image_tk = ImageTk.PhotoImage(image, master=self.parent)
+        return self.image_tk
+
+    def clear(self):
+        self.image_tk = None
