483beaba2dabd606597e3933c06395b73c3ceca2
Liven Maxim <maxlevs@mail.ru>
2025-05-31 07:50:24 +0300

    chore: add execution time measurement

--- application/basins_drawer_service.py
+++ application/basins_drawer_service.py
@@ -3,6 +3,7 @@
 from domain.image_tile import *
 from domain.math_equation import *
 from domain.root_color import *
+import common.utils as utils
 
 
 class BasinsDrawerService:
@@ -21,14 +22,17 @@ class BasinsDrawerService:
         self.math_equation = MathEquation(roots)
         self.__get_color_by_cluster_and_iterations_vector = np.vectorize(self.__get_color_by_cluster_and_iterations)
 
+    @utils.timing
     def draw(self, tile: ImageTile) -> Image:
         _, labels = self.get_roots_out_of_screen_points(tile)
         return self.create_image(labels, tile)
 
+    @utils.timing
     def get_roots_out_of_screen_points(self, tile: ImageTile):
         z0s = tile.get_virtual_points_complex()
         return self.math_equation.try_find_root_from(z0s, self.max_iterations)
 
+    @utils.timing
     def create_image(self, labels, tile: ImageTile):
         colors_by_rows = np.column_stack(self.__get_color_by_cluster_and_iterations_vector(labels))
         colors_by_rows = colors_by_rows.astype(np.uint8).reshape((tile.image_x_max, tile.image_y_max, 3))
